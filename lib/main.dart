a11ced421884c63867afeb22d59ed544550b46b8
aprisyta <aprisyta10@gmail.com>
2018-09-27 00:08:15 +0530

    Rendered list of locations

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'location_detail.dart';
 import './model/location.dart';
 import './mocks/mock_location.dart';
-
-final Location mockLocation = MockLocation.FetchAny();
+import 'location_list.dart';
+final List<Location> mockLocations = MockLocation.fetchAll();
 
 void main() => runApp(MaterialApp(
-  home: new LocationDetail(mockLocation)
+//  home: new LocationDetail(mockLocation)
+  home: new LocationList(mockLocations),
 ));
